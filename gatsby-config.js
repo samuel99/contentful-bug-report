e0ceee4eca1a47c6c211d80d3a3a5f898825ce7c
samuelconsid <samuel.milton@consid.se>
2022-06-02 10:11:18 +0200

    Update config and packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: "spaceID",
+        accessToken: "accessToken",
+      },
+    },
+  ],
+};
